fb454e1c934dcf0029c9598cb30d548dcc9b4448
ethancoj <152013816+ethancoj@users.noreply.github.com>
2025-01-11 02:40:44 +0800

    Add files via upload

--- app/src/main/java/com/example/sustainablelivingguide/MainActivity.java
+++ app/src/main/java/com/example/sustainablelivingguide/MainActivity.java
@@ -1,1 +1,168 @@
+package com.example.sustainablelivingguide;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
+
+    Button btn1, btn2, btn3, btn4;
+
+    private NewsApiClient newsApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btn4 = findViewById(R.id.btn_4);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+
+        newsApiClient = new NewsApiClient("ebb1998f09924ce585edcfb87e2bb2f7");
+
+        setRecyclerView();
+        getNews();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    void setRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean show) {
+        if (show)
+            progressIndicator.setVisibility(View.VISIBLE);
+        else
+            progressIndicator.setVisibility(View.INVISIBLE);
+    }
+
+    private void getNews() {
+        changeInProgress(true);
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q("nature AND sustainability")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }
+        );
+    }
+
+    private void getProjects() {
+        changeInProgress(true);
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q("diy AND crafts OR nature") // query
+                        .domains("makezine.com")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        runOnUiThread(() -> {
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("NewsAPI", throwable.getMessage());
+                        changeInProgress(false);
+                    }
+                }
+        );
+    }
+
+    private void getEvents() {
+        changeInProgress(true);
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q("bitcoin") // query
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        runOnUiThread(() -> {
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("NewsAPI", throwable.getMessage());
+                        changeInProgress(false);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onClick(View v) {
+       if (v.getId() == R.id.btn_1) {
+           getNews();
+       } else if (v.getId() == R.id.btn_3) {
+           getProjects();
+       } else if (v.getId() == R.id.btn_4) {
+           getEvents();
+        }
+    }
+}
